4af89aa1f59f0315603ccb2c6d0679230a516045
rohitnakashe <52915840+rohitnakashe@users.noreply.github.com>
2024-09-11 23:15:42 -0700

    Create Moduleadd.v

---
+++ Day2/Moduleadd.v
@@ -0,0 +1,12 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+	wire x,y;
+    
+    add16 inst1 (.a(a[15:0]), .b(b[15:0]), .cin(1'b0), .sum(sum[15:0]), .cout(x));
+	add16 inst2 (.a(a[31:16]), .b(b[31:16]), .cin(x), .sum(sum[31:16]), .cout(y));
+
+
+endmodule
