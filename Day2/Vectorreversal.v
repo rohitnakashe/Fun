ff4e8a64eb00cd0ee7d2288568037a80ee4e99c3
rohitnakashe <52915840+rohitnakashe@users.noreply.github.com>
2024-09-11 21:30:21 -0700

    Create Vectorreversal.v

---
+++ Day2/Vectorreversal.v
@@ -0,0 +1,7 @@
+module top_module( 
+    input [7:0] in,
+    output [7:0] out
+);
+    assign out [7:0] = {in[0],in[1],in[2],in[3],in[4],in[5],in[6],in[7]};
+    //assign out [7:0] = in [0:7]; does not work because Verilog does not allow vector bit ordering to be flipped.
+endmodule
